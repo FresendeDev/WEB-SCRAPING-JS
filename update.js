77614139c9dbbc78f440b046a2cf9c6f0fc06937
Fabio Resende <fabio.resende@gmail.com>
2022-10-27 09:50:36 -0300

    ajustando funcao update

--- update.js
+++ update.js
@@ -8,7 +8,13 @@ const JSONUpdate = (filePath, newData, encoding = "utf-8") => {
     try {
       const data = await JSONRead(filePath, encoding);
       data.push(newData);
-
+      // const result = { ...data, ...newData };
+      // const result = data;
+      // console.log("data = ", data);
+      // console.log("newData = ", newData, "fim newdata");
+      // const result = data.push(newData);
+      // await JSONWrite(filePath, result, encoding);
+      await JSONWrite(filePath, data, encoding);
       resolve(data);
     } catch (e) {
       reject(e);
